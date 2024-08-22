9308ea11f224301f4110d43368c6f6843c907a55
LuisanaGarello <164653068+lu98ojjpijpjphih@users.noreply.github.com>
2024-08-22 08:30:38 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,42 @@
+let frutaVerdura = []
+console.log (frutaVerdura.length)
+let kilos = []
+console.log (kilos.length)
+
+for (let index = 0; index < 7; index++) {
+      console.log ("Ingrese una fruta")
+      frutaVerdura.push [index]
+      console.log ("Ingresa su kilo")
+      kilos.push [index]
+      console.log ("Ingrese una verdura")
+      frutaVerdura.push [index]
+      console.log ("Ingresa su kilo")
+      kilos.push [index]
+     
+ }
+
+ 
+ if (frutaVerdura = "tomate") {
+    console.log ("Hay stock" + kilos)
+} else {
+    console.log ("No hay stock")
+}
+
+//Actualización
+frutaVerdura[0] = ("Este se actualizo a" + kilo [0] + 20)
+frutaVerdura[1] = ("Este se actualizo a" + kilo [1] + 20)
+
+
+for (let index = 0; index > kilo.length; index++) {
+   if (kilo > 20) {
+       console.log (frutaVerdura + kilo)
+   } else {
+       console.log ("Este no esta en stock")
+   }
+    
+}
+
+
+
+
+
